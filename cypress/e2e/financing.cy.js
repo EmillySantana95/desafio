f0013aee194bdbaae8a3f114a56722bb17cd891b
shiraneim <andrenaritajun@gmail.com>
2023-10-09 14:56:53 -0300

    atualização dos testes para o desafio

--- cypress/e2e/financing.cy.js
+++ cypress/e2e/financing.cy.js
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 
-describe('Dev Finance', () => {
+describe('Cadastro de Transações', () => {
 
-	it('Deve cadastrar uma transação com valor positivo e validar informações da transação', () => {
+	it('Deve cadastrar uma transação com valor 100,00 e validar a linha na tabela informações da transação', () => {
 		cy.visit('https://devfinance-agilizei.netlify.app/#')
 		cy.get('a[class="button new"]').should('be.visible').click()
 
@@ -13,23 +13,8 @@ describe('Dev Finance', () => {
 		cy.get('div[class="input-group actions"] > button').should('be.visible').click()
 
 		cy.get('tr[data-index="0"] > td').eq(0).should('be.visible').and('have.text', 'Transação com valor positivo')
-		cy.get('tr[data-index="0"] > td').eq(1).should('be.visible').and('have.text', 'R$\u00a0100,00')
-		cy.get('tr[data-index="0"] > td').eq(2).should('be.visible').and('have.text', '10/08/2023')
-	})
-
-	it('Deve cadastrar uma transação com valor negativo e validar informações da transação', () => {
-		cy.visit('https://devfinance-agilizei.netlify.app/#')
-		cy.get('a[class="button new"]').click()
-
-		cy.get('div[class="modal"]').should('be.visible')
-		cy.get('input[id="description"]').type('Transação com valor negativo')
-		cy.get('input[id="amount"]').type("-100.00")
-		cy.get('input[id="date"]').type('2023-08-10')
-		cy.get('div[class="input-group actions"] > button').click()
-
-		cy.get('tr[data-index="0"] > td').eq(0).should('have.text', 'Transação com valor negativo')
-		cy.get('tr[data-index="0"] > td').eq(1).should('have.text', '-R$\u00a0100,00')
-		cy.get('tr[data-index="0"] > td').eq(2).should('have.text', '10/08/2023')
+		cy.get('tr[data-index="0"] > td').eq(1).should('be.visible').and('contain.text', '100,00')
+		cy.get('tr[data-index="0"] > td').eq(2).should('be.visible').and('have.text', '10/08/2024')
 	})
 
 	it('Deve cadastrar uma transação com valor positivo (Salario mês 5000,00) e uma com valor negativo (Parcela carro 1500,00) e validar valor total', () => {
@@ -60,10 +45,10 @@ describe('Dev Finance', () => {
 
 		cy.get('p[id="incomeDisplay"]').should('have.text', 'R$\u00a05.000,00')
 		cy.get('p[id="expenseDisplay"]').should('have.text', '-R$\u00a01.500,00')
-		cy.get('p[id="totalDisplay"]').should('have.text', 'R$\u00a03.500,00')
+		cy.get('p[id="totalDisplay"]').should('have.text', 'R$\u00a02.500,00')
 	})
 
-	it('Deve inserir 2 transações (Salario mês e VR) após, excluir a transação VR', () => {
+	it('Deve inserir 2 transações quaisquer (Salario mês e VR) após, excluir a transação VR', () => {
 		cy.visit('https://devfinance-agilizei.netlify.app/#')
 		cy.get('a[class="button new"]').click()
 
@@ -77,7 +62,7 @@ describe('Dev Finance', () => {
 		cy.get('tr[data-index="0"] > td').eq(1).should('contain.text', 'R$\u00a05.000,00')
 		cy.get('tr[data-index="0"] > td').eq(2).should('have.text', '10/08/2023')
 
-		cy.get('a[class="button new"]').click()
+		cy.get('a[class="button ned"]').click()
 
 		cy.get('div[class="modal"]').should('be.visible')
 		cy.get('input[id="description"]').type('Vale Refeição')
@@ -90,5 +75,50 @@ describe('Dev Finance', () => {
 		cy.get('tr[data-index="1"] > td').eq(2).should('have.text', '11/08/2023')
 
 		cy.get('tr[data-index="1"] > td > img').click().should('not.exist')
+	})
+
+	it('Deve cadastrar uma transação com valor 100,00 e validar os blocos de entrada, saída e valor total', () => {
+		cy.visit('https://devfinance-agilizei.netlify.app/#')
+		cy.get('a[class="button new"]').should('be.visible').click()
+
+		cy.get('div[class="modal"]').should('be.visible')
+		cy.get('input[id="description"]').should('be.visible').type('Transação com valor positivo')
+		cy.get('input[id="amount"]').should('be.visible').type("100.00")
+		cy.get('input[id="date"]').should('be.visible').click('2023-08-10')
+		cy.get('div[class="input-group actions"] > button').should('be.visible').click()
+
+		cy.get('p[id="incomeDisplay"]').should('have.text', 'R$\u00a0100,00')
+		cy.get('p[id="expenseDisplay"]').should('have.text', 'R$\u00a00,00')
+		cy.get('p[id="totalDisplay"]').should('have.text', 'R$\u00a0100,00')
+	})
+
+	it('Deve cadastrar uma transação com valor -100,00 e validar os blocos de entrada, saída e valor total', () => {
+		cy.visit('https://devfinance-agilizei.netlify.app/#')
+		cy.get('a[class="button new"]').should('be.visible').click()
+
+		cy.get('div[class="modal"]').should('be.visible')
+		cy.get('input[id="description"]').should('be.visible').type('Transação com valor positivo')
+		cy.get('input[id="amount"]').should('be.visible').type("-100.00")
+		cy.get('input[id="date"]').should('be.visible').type('2023-08-10')
+		cy.get('div[class="input-group actions"] > button').should('be.visible').click()
+
+		cy.get('p[id="incomeDisplay"]').should('have.text', 'R$ 0,00')
+		cy.get('p[id="expenseDisplay"]').should('have.text', '-R$ 100,00')
+		cy.get('p[id="totalDisplay"]').should('have.text', '-R$ 100,00')
+	})
+
+	it('Deve cadastrar uma transação com valor -100,00 e validar a linha na tabela informações da transação', () => {
+		cy.visit('https://devfinance-agilizei.netlify.app/#')
+		cy.get('a[class="button new"]').click()
+
+		cy.get('div[class="modal"]').should('be.visible')
+		cy.get('input[id="description"]').type('Transação com valor negativo')
+		cy.get('input[id="amount"]').type("-100.00")
+		cy.get('input[id="date"]').type('2023-08-10')
+		cy.get('div[class="input-group actions"] > button').click()
+
+		cy.get('tr[data-index="0"] > td').eq(0).should('have.text', 'Transação com valor negativo')
+		cy.get('tr[data-index="0"] > td').eq(2).should('have.text', '10/08/2023')
+		cy.get('tr[data-index="0"] > td').eq(3).should('have.text', 'R$\u00a0100.00')
 	})
 })
